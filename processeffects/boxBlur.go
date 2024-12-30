5608e28b31562e564d84f9b80ec99e2d02bbc7cf
Andrew <67875282+andrewchu16@users.noreply.github.com>
2024-12-29 23:26:11 -0500

    Utilize WaitGroups to speed up BoxBlur effect

--- processeffects/boxBlur.go
+++ processeffects/boxBlur.go
@@ -1,8 +1,11 @@
 package processeffects
 
-import "tinyraytracer/geometry"
+import (
+    "tinyraytracer/geometry"
+    "sync"
+)
 
-func BoxBlur(bufp *[][]geometry.Vec3, blurWidth int) *[][]geometry.Vec3 {
+func BoxBlur(bufp *[][]geometry.Color, blurWidth int) *[][]geometry.Color {
 	if blurWidth <= 0 {
 		return bufp
 	}
@@ -17,29 +20,30 @@ func BoxBlur(bufp *[][]geometry.Vec3, blurWidth int) *[][]geometry.Vec3 {
         newBuf[i] = make([]geometry.Vec3, width)
     }
 
-    for y := range height {
-        for x := range width {
-            // Blur the image
-            newCol := geometry.NewVec3(0, 0, 0)
-
-            count := 0
-            for j := -blurWidth; j <= blurWidth; j++ {
-                for i := -blurWidth; i <= blurWidth; i++ {
-                    nx := x + i
-                    ny := y + j
-
-                    if nx >= 0 && nx < width && ny >= 0 && ny < height {
-                        newCol.Add(buf[ny][nx])
-                        count++
+    var wg sync.WaitGroup
+    for y := 0; y < height; y++ {
+        wg.Add(1)
+        go func(y int) {
+            defer wg.Done()
+            for x := 0; x < width; x++ {
+                newCol := geometry.NewVec3(0, 0, 0)
+                count := 0
+                for j := -blurWidth; j <= blurWidth; j++ {
+                    for i := -blurWidth; i <= blurWidth; i++ {
+                        nx := x + i
+                        ny := y + j
+                        if nx >= 0 && nx < width && ny >= 0 && ny < height {
+                            newCol.Add(buf[ny][nx])
+                            count++
+                        }
                     }
                 }
+                newCol.Div(float64(count))
+                newBuf[y][x] = newCol
             }
-
-            newCol.Div(float64(count))
-
-            newBuf[y][x] = newCol
-        }
+        }(y)
     }
+    wg.Wait()
 
     return &newBuf
 }
