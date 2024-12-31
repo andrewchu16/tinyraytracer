33b5248b5e2a3779cf58acda1816fb220721a4e5
Andrew <67875282+andrewchu16@users.noreply.github.com>
2024-12-30 21:36:43 -0500

    Remove excess filters

--- processing/gaussianBlur.go
+++
@@ -1,9 +0,0 @@
-package processing
-
-import "tinyraytracer/geometry"
-
-// Gausian Blur
-
-func GaussianBlur(bufp *[][]geometry.Vec3, blurWidth int) *[][]geometry.Vec3 {
-	return bufp;
-}
--- processing/weightedFilter.go
+++
@@ -1,9 +0,0 @@
-package processing
-
-import "tinyraytracer/geometry"
-
-// Weighted Filter
-
-func WeightedFilter(bufp *[][]geometry.Vec3) *[][]geometry.Vec3 {
-	return bufp;
-}
